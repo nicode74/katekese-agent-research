73fa61d0a42e99f1e495807f637bdc1ca1d0eb11
nicode74 <nicolast.nicolast.74@gmail.com>
2026-06-21 02:47:47 +0700

    refactor: remove legacy mamba installation scripts and update gitignore for virtual environment patterns

--- install_mamba.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest > micromamba.tar.bz2
-tar -xvjpf micromamba.tar.bz2 bin/micromamba
-rm micromamba.tar.bz2
-./bin/micromamba create -p ./env311 python=3.11 -c conda-forge -y
--- run_option_a.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./env311/bin/pip install langchain-google-genai python-dotenv requests
-./env311/bin/python src/indexer/upload_gemini_batched.py
